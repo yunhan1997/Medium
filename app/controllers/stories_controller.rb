4bdd9763a1580df977f37a406571f1e8f2b15981
han <victory0246@gmail.com>
2022-11-24 16:12:28 +0800

    new story and edit

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,52 @@
+class StoriesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :find_story, only: [:edit, :update, :show, :destory]
+
+
+    def index
+        @stories = current_user.stories.order(created_at: :desc)
+    
+    end
+
+    def new
+        @story = current_user.stories.new
+    end
+
+
+    def create
+        @story = current_user.stories.new(story_params)
+    
+        if @story.save
+            redirect_to stories_path, notice: '新增成功'
+
+        else
+            render :new
+        
+        end
+    end
+
+
+    def edit
+    end
+
+    def update
+        if @story.update(story_params)
+            redirect_to stories_path,notice: '更新成功'
+        else
+            render :edit
+        end
+    end
+    
+    
+    
+    
+    private
+    def find_story
+        @story = current_user.stories.find(params[:id])
+    end
+    
+    
+    def story_params
+        params.require(:story).permit(:title, :content)
+    end
+end
