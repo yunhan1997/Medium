66ca40ebf239d6ba46fa7119219f8a530aa69489
han <victory0246@gmail.com>
2022-12-05 17:11:53 +0800

    stimulus install and demo

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   get '@:username/:story_id', to: 'pages#show', as: 'story_page'
   get '@:username', to: 'pages#user', as: 'user_page'
 
+
+  get "/demo" , to: 'pages#demo'
+
   root 'pages#index'
 end
