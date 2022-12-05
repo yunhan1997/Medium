99d35e488877c413f2d1d19f47496a4a4e4a3f9c
han <victory0246@gmail.com>
2022-12-05 10:27:35 +0800

    index (story list)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
 
     def index
-    
+        @stories = Story.order(created_at: :desc).includes(:user)
     end
 
 
+    def show
+    end
+
+
+    def user
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   
   resources :stories
 
+  get '@:username/:story_id', to: 'pages#show', as: 'story_page'
+  get '@:username', to: 'pages#user', as: 'user_page'
+
   root 'pages#index'
 end
